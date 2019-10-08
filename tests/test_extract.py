8d352512b778c09a3bb6a7f6aa34f6653321ad73
Niels Buwen <buwen@stud.uni-heidelberg.de>
2019-10-08 13:35:16 +0200

    adjust test

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,5 +1,4 @@
-from skillbridge.client.extract import parse_all_function, functions_by_prefix,\
-    Function, WHITELIST
+from skillbridge.client.extract import parse_all_function, Function
 
 
 def test_parse_all():
@@ -7,24 +6,3 @@ def test_parse_all():
     assert functions
 
     assert all(isinstance(func, Function) for func in functions)
-
-
-def test_by_prefix():
-    groups = functions_by_prefix()
-
-    for group in WHITELIST:
-        assert group in groups
-
-        assert all(isinstance(func, Function) for func in groups[group])
-
-
-def test_aliases_are_mapped_even_when_prefix_not_whitelisted():
-    functions = functions_by_prefix()['get']
-
-    assert any(func.name == 'getCurrentWindow' for func in functions)
-
-
-def test_not_whitelisted_prefix_not_present():
-    functions = functions_by_prefix()['get']
-
-    assert all(func.name != 'getValue' for func in functions)
