75509f566b1d8ad7b63ca4a117ba0db0ce26e51c
tiffmay <tiff.mayberry@gmail.com>
2021-07-14 11:50:20 -0500

    #2 copied python_server.py to be in same location as  python_server.il
    python_server.il expects python_server.py to be along side it,

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -6,4 +6,7 @@ cp -f "${RECIPE_DIR}/../skillbridge.init.il" \
 cp -f "${RECIPE_DIR}/../skillbridge/server/python_server.il" \
        "${PREFIX}/lib/skill/skillbridge/python_server.il"
 
+cp -f "${RECIPE_DIR}/../skillbridge/server/python_server.py" \
+       "${PREFIX}/lib/skill/skillbridge/python_server.py"
+
 $PYTHON setup.py install
