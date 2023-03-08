8c38f3212240c55b9006b8fa201004d9db5d45d3
Niels Buwen <dev@niels-buwen.de>
2023-03-08 10:37:46 +0100

    Pull version number from single source

--- update_version.py
+++ update_version.py
@@ -4,8 +4,6 @@ from sys import argv
 _, ref = argv
 *_, version = ref.split('/')
 here = Path(__file__).parent
-setup_cfg = here / 'setup.cfg'
 version_py = here / 'skillbridge' / 'version.py'
 
-setup_cfg.write_text(setup_cfg.read_text().replace('version = 1dev', f'version = {version}'))
 version_py.write_text(f"__version__ = '{version}'\n")
